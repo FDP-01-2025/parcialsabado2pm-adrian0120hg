c53e80e780b22c556373a4cd89afbab33e2eb1de
adrian0120hg <00032125@uca.edu.sv>
2025-06-07 15:52:14 -0600

    Add files via upload

---
+++ dowhile.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int numero,pares=0;
+
+cout << "Digite el valor del numero" << endl; cin >> numero;
+
+if ( numero = numero % 2 != 0)
+{
+    cout << "el numero es impar por lo tanto no puede inicializar en 0 " << endl;
+}
+
+
+do
+{
+   cout << "Estos son los numeros pares: " << pares + 2 << endl; cin >> pares;
+
+ 
+  
+    
+} while (numero >= numero);
+
+cout << "estos son los numeros pares: " << pares << endl;
+
+    return 0;
+}
---
+++ ejercicio.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int altura;
+cout << "Ingrese la altura de la piramide" << endl; cin >> altura;
+
+for (int i = 0; i < altura; i--)
+{
+ 
+
+cout << "#############" << endl;
+cout << " ##########" << endl;
+cout << " #########" << endl;
+cout <<   "#####" << endl;
+cout <<    "###" << endl;
+cout <<    "##" << endl;
+} 
+ 
+
+    return 0;
+}
